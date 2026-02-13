ab5b5acc651d8a30095f35af3e98b6a08cae2d0a
hferraud <ferraudhugo@gmail.com>
2026-02-13 17:52:47 +0100

    feat: add vagrant and virtualbox install script alongside basic vagrantfile

---
+++ .script/vagrant_install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+VERSION="2.4.9"
+URL="https://releases.hashicorp.com/vagrant/${VERSION}/vagrant_${VERSION}_linux_amd64.zip"
+VAGRANT_TMP=/tmp/vagrant_${VERSION}.zip
+VAGRANT_PATH=/opt/vagrant/${VERSION}
+BIN_PATH=/usr/local/bin/vagrant
+
+echo "Downloading vagrant..."
+wget -q ${URL} -O ${VAGRANT_TMP}
+
+rm -rf ${VAGRANT_PATH}
+
+echo "Installing vagrant..."
+mkdir -p ${VAGRANT_PATH}
+unzip -o -q ${VAGRANT_TMP} -d ${VAGRANT_PATH}
+
+rm -rf ${VAGRANT_TMP}
+
+ln -sf "${VAGRANT_PATH}/vagrant" "${BIN_PATH}"
+
+echo "Done"
+vagrant --version
---
+++ .script/virtualbox_install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo gpg --dearmor -o /usr/share/keyrings/oracle-virtualbox.gpg
+echo "deb [signed-by=/usr/share/keyrings/oracle-virtualbox.gpg] http://download.virtualbox.org/virtualbox/debian bookworm contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+sudo apt update
+sudo apt install vitualbox build-essential dkms linux-headers-amd64 -y
