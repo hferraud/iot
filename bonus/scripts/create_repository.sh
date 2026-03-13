f30ec309518b11319ff1b63b4141e955df660f4d
hferraud <ferraudhugo@gmail.com>
2026-03-13 19:46:06 +0100

    feat: add bonus

---
+++ bonus/scripts/create_repository.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+NAMESPACE="gitlab"
+REPO_NAME="iot_deployment_hferraud"
+GITHUB_REPO="https://github.com/hferraud/${REPO_NAME}.git"
+GITLAB_REPO="http://gitlab.example.com/root/${REPO_NAME}.git"
+WORKDIR="/tmp/${REPO_NAME}"
+
+GITLAB_TOOLBOX_POD=$(kubectl get pods -n ${NAMESPACE} -l app=toolbox -o jsonpath='{.items[0].metadata.name}')
+
+GITLAB_PAT=$(kubectl exec -n ${NAMESPACE} $GITLAB_TOOLBOX_POD -- gitlab-rails runner \
+  "t = User.find_by_username('root').personal_access_tokens.create(
+    name: 'script-token3',
+    scopes: ['api', 'write_repository', 'read_repository'],
+    expires_at: 1.year.from_now
+  ); puts t.errors.any? ? t.errors.full_messages : t.token")
+
+#echo $GITLAB_PAT
+#
+#curl --request POST "http://gitlab.example.com/api/v4/projects" \
+#--header "PRIVATE-TOKEN: $GITLAB_PAT" \
+#--header "Content-Type: application/json" \
+#--data '{"name": "iot_deployment_hferraud", "visibility": "public"}'
+
+rm -rf ${WORKDIR}
+git clone ${GITHUB_REPO} ${WORKDIR}
+cd ${WORKDIR}
+git remote set-url origin http://root:${GITLAB_PAT}@gitlab.example.com/root/${REPO_NAME}
+git push origin main
+
--- bonus/scripts/setup.sh
+++ bonus/scripts/setup.sh
@@ -8,6 +8,7 @@ k3d cluster create $K3D_CLUSTER_NAME -c config/k3d.yaml
 kubectl apply -f config/namespace.yaml
 
 helm install gitlab gitlab/gitlab \
+  --set global.edition=ce \
   --set global.hosts.domain=example.com \
   --set global.hosts.externalIP=0.0.0.0 \
   --set global.hosts.https=false \
@@ -18,9 +19,11 @@ helm install gitlab gitlab/gitlab \
 
 kubectl apply -f config/ingress.yaml
 
-#kubectl apply -n $NAMESPACE_ARGOCD --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-#kubectl patch svc argocd-server -n $NAMESPACE_ARGOCD -p '{"spec": {"type": "LoadBalancer"}}'
-#
-#kubectl apply -f config/application.yaml
+kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl -n argocd patch deployment argocd-server \
+  --type='json' \
+  -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value":"--insecure"}]'
+
+kubectl apply -f config/application.yaml
 
 
