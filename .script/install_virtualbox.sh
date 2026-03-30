e5e2eb620d82d66db8d1d939d3f56ea6d8418029
Sylvain Luiset <syluiset@z3r5p4.42lyon.fr>
2026-03-30 18:55:08 +0200

    update install vbox

--- .script/install_virtualbox.sh
+++ .script/install_virtualbox.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo gpg --dearmor -o /usr/share/keyrings/oracle-virtualbox.gpg
+sudo wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo gpg --dearmor -o /usr/share/keyrings/oracle-virtualbox.gpg
 echo "deb [signed-by=/usr/share/keyrings/oracle-virtualbox.gpg] http://download.virtualbox.org/virtualbox/debian bookworm contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
 sudo apt update
 sudo apt install virtualbox-7.2 build-essential dkms linux-headers-amd64 -y
